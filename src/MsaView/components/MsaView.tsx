89046885ae9c7781ae973b508a367371bd683e3c
Colin <colin.diesh@gmail.com>
2021-02-01 16:02:56 -0700

    Updates

--- src/MsaView/components/MsaView.tsx
+++ src/MsaView/components/MsaView.tsx
@@ -1,11 +1,9 @@
 import PluginManager from "@jbrowse/core/PluginManager";
 import ImportFormComponent from "./ImportForm";
+import colorSchemes from "./colorSchemes";
 
-// import colorSchemes from "./colorSchemes";
-
-// const defaultColorScheme = "maeditor";
-
-// const colorScheme = colorSchemes[defaultColorScheme];
+const defaultColorScheme = "maeditor";
+const colorScheme = colorSchemes[defaultColorScheme];
 
 export default (pluginManager: PluginManager) => {
   const { jbrequire } = pluginManager;
@@ -42,55 +40,64 @@ export default (pluginManager: PluginManager) => {
     );
   });
 
-  // const space = 10;
-  // const MSA = observer(({ model }: { model: any }) => {
-  //   const { width, height, msa, tree } = model;
-  //   console.log({ tree, msa });
-  //   return (
-  //     <>
-  //       {tree.leaves().map(node => {
-  //         const {
-  //           x,
-  //           y,
-  //           data: { name },
-  //         } = node;
-  //         const ypos = y + 300;
-  //         const curr = msa.alns.find(aln => aln.id === name);
-  //         return curr.seq.split("").map((letter, index) => {
-  //           const color = colorScheme[letter];
-  //           // const contrast = color ? theme.palette.getContrastText(color) : 'none'
-  //           return (
-  //             <React.Fragment key={`${name}-${index}`}>
-  //               <rect
-  //                 x={ypos + index * space}
-  //                 y={x - space}
-  //                 width={space}
-  //                 height={space}
-  //                 fill={color || "none"}
-  //               />
-  //               <text x={ypos + index * space} y={x}>
-  //                 {letter}
-  //               </text>
-  //             </React.Fragment>
-  //           );
-  //         });
-  //       })}
-  //     </>
-  //   );
-  // });
+  const space = 10;
+  const MSA = observer(({ model }: { model: any }) => {
+    const { msa, tree, theme } = model;
+    if (!msa) {
+      return null;
+    }
+
+    return (
+      <>
+        {tree.leaves().map((node: any) => {
+          const {
+            x,
+            y,
+            data: { name },
+          } = node;
+          const ypos = y + 300;
+          const curr = msa.alns.find((aln: any) => aln.id === name);
+          return curr.seq.split("").map((letter: string, index: number) => {
+            const color = (colorScheme as any)[letter];
+            const contrast = color
+              ? theme.palette.getContrastText(color)
+              : "none";
+            return (
+              <React.Fragment key={`${name}-${index}`}>
+                <rect
+                  x={ypos + index * space}
+                  y={x - space}
+                  width={space}
+                  height={space}
+                  fill={color || "none"}
+                />
+                <text x={ypos + index * space} y={x} fill={contrast}>
+                  {letter}
+                </text>
+              </React.Fragment>
+            );
+          });
+        })}
+      </>
+    );
+  });
 
   return observer(({ model }: { model: any }) => {
-    const { width, height, initialized, margin, totalHeight } = model;
+    const { height, initialized, margin, totalHeight } = model;
+    const treeWidth = 300;
 
     if (!initialized) {
       return <ImportForm model={model} />;
     }
 
     return (
-      <div style={{ height, width, overflow: "auto" }}>
-        <svg style={{ height: totalHeight, width }}>
+      <div style={{ height, overflow: "auto" }}>
+        <svg style={{ height: totalHeight, width: treeWidth }}>
           <g transform={`translate(${margin.left}, ${margin.top})`}>
             <TreeCanvas model={model} />
+            <g transform={`translate(${treeWidth} 0)`}>
+              <MSA model={model} />
+            </g>
           </g>
         </svg>
       </div>
