50cb7b84ce6f275ed3ac19767858e18af701e379
Colin <colin.diesh@gmail.com>
2021-03-16 15:31:29 -0400

    Updates

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,21 @@
 import { observer } from "mobx-react";
 import { MSAView, MSAModel } from "msaview";
+import { createJBrowseTheme } from "@jbrowse/core/ui/theme";
+
+import { ThemeProvider } from "@material-ui/core/styles";
 
 function App() {
+  const theme = createJBrowseTheme();
   const model = MSAModel.create({ id: `${Math.random()}`, type: "MsaView" });
-  model.setWidth(1500);
+  model.setWidth(1800);
 
-  return <MSAView model={model} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={{ border: "1px solid black", margin: 20 }}>
+        <MSAView model={model} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default observer(App);
