83c08ead7661b6c4f38f7227494561cf16199d91
Colin Diesh <colin.diesh@gmail.com>
2021-06-02 02:30:32 -0400

    Drag resize handle (#13)
    * Add some typography elements to make text look standard
    
    * Add drag resize handle
    
    * Add yarn start command
    
    * Updates
    
    * Add some descriptions
    
    * Bump github node version

--- lib/src/components/MSAView.tsx
+++ lib/src/components/MSAView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { MsaViewModel } from '../model'
 import { observer } from 'mobx-react'
@@ -10,6 +10,46 @@ import MSACanvas from './MSACanvas'
 import Header from './Header'
 export default observer(({ model }: { model: MsaViewModel }) => {
   const { done, initialized } = model
+  const [mouseDown, setMouseDown] = useState(false)
+  const [cropMouseDown, setCropMouseDown] = useState(false)
+
+  // this has the effect of making the tree smaller
+  useEffect(() => {
+    if (mouseDown) {
+      const listener = (event: MouseEvent) =>
+        model.setTreeWidth(model.treeAreaWidth + event.movementX)
+
+      const listener2 = () => setMouseDown(false)
+
+      document.addEventListener('mousemove', listener)
+      document.addEventListener('mouseup', listener2)
+      return () => {
+        document.removeEventListener('mousemove', listener)
+        document.removeEventListener('mouseup', listener2)
+      }
+    }
+    return () => {}
+  }, [mouseDown])
+
+  // this has the effect of just "cropping" the tree area
+  useEffect(() => {
+    if (cropMouseDown) {
+      const listener = (event: MouseEvent) => {
+        model.setNameWidth(model.nameWidth + event.movementX)
+        model.setTreeWidth(model.treeAreaWidth + event.movementX)
+      }
+
+      const listener2 = () => setCropMouseDown(false)
+
+      document.addEventListener('mousemove', listener)
+      document.addEventListener('mouseup', listener2)
+      return () => {
+        document.removeEventListener('mousemove', listener)
+        document.removeEventListener('mouseup', listener2)
+      }
+    }
+    return () => {}
+  }, [cropMouseDown])
 
   if (!initialized) {
     return <ImportForm model={model} />
@@ -28,6 +68,28 @@ export default observer(({ model }: { model: MsaViewModel }) => {
           }}
         >
           <TreeCanvas model={model} />
+          <div>
+            <div
+              onMouseDown={() => {
+                setCropMouseDown(true)
+              }}
+              style={{
+                cursor: 'ew-resize',
+                height: '50%',
+                border: '3px solid rgba(40,200,40)',
+              }}
+            />
+            <div
+              onMouseDown={() => {
+                setMouseDown(true)
+              }}
+              style={{
+                cursor: 'ew-resize',
+                height: '50%',
+                border: '3px solid rgba(200,40,40)',
+              }}
+            />
+          </div>
           <MSACanvas model={model} />
         </div>
       </div>
