48626d49e6e506fdca3b5ee14c7d2820f18ad760
Colin <colin.diesh@gmail.com>
2021-06-15 01:50:07 -0400

    Testing

--- app/src/App.js
+++ app/src/App.js
@@ -17,19 +17,35 @@ import { ThemeProvider } from "@material-ui/core/styles";
 const urlParams = new URLSearchParams(window.location.search);
 const val = urlParams.get("data");
 
-const model = MSAModel.create({
+const global = MSAModel.create({
   type: "MsaView",
   height: 550,
   ...JSON.parse(val),
 });
 
-model.setWidth(window.innerWidth);
+global.setWidth(window.innerWidth);
+
+onSnapshot(
+  global,
+  throttle((snap) => {
+    const url = new URL(window.document.URL);
+    url.searchParams.set("data", JSON.stringify(snap));
+    window.history.replaceState(null, "", url.toString());
+  }, 500)
+);
+
+// Handle window resizing
+window.addEventListener("resize", () => {
+  global.setWidth(window.innerWidth);
+});
 
 const ProteinPanel = observer(({ model }) => {
   const ref = useRef();
+  const tooltipRef = useRef();
   const [type, setType] = useState("cartoon");
   const [res, setRes] = useState([]);
   const [stage, setStage] = useState();
+  const [annotation, setAnnotation] = useState();
   const { selected, mouseCol } = model;
 
   useEffect(() => {
@@ -44,11 +60,7 @@ const ProteinPanel = observer(({ model }) => {
       // Handle window resizing
       window.addEventListener("resize", () => {
         stage.handleResize();
-        model.setWidth(window.innerWidth);
       });
-
-      // Code for example: interactive/hover-tooltip
-      // create tooltip element and add to document body
 
       const res = await Promise.all(
         selected.map((selection) =>
@@ -57,9 +69,9 @@ const ProteinPanel = observer(({ model }) => {
       );
       setRes(res);
 
-      stage.signals.hovered.add(function (pickingProxy) {
+      stage.signals.hovered.add((pickingProxy) => {
         if (pickingProxy && (pickingProxy.atom || pickingProxy.bond)) {
-          var atom = pickingProxy.atom || pickingProxy.closestBondAtom;
+          const atom = pickingProxy.atom || pickingProxy.closestBondAtom;
           model.setMouseoveredColumn(
             atom.resno,
             atom.chainname,
@@ -79,24 +91,40 @@ const ProteinPanel = observer(({ model }) => {
   }, [type, res]);
 
   useEffect(() => {
-    if (mouseCol !== undefined) {
-      res.forEach((elt) => {
-        elt.addRepresentation("label", { sele: `@${mouseCol}` });
-        stage.autoView();
-      });
-    }
-  }, [res, mouseCol]);
+    const annots = [];
+    res?.forEach((elt, index) => {
+      if (annotation) {
+        elt.removeAnnotation(annotation[index]);
+      }
+      const tooltip = tooltipRef.current;
+      const cp = elt.structure.getResidueProxy(mouseCol);
+      const ap = elt.structure.getAtomProxy();
+      ap.index = cp.atomOffset;
+      var elm = document.createElement("div");
+      elm.innerText = "hello";
+      elm.style.color = "black";
+      elm.style.backgroundColor = "red";
+      elm.style.padding = "8px";
 
-  useEffect(() => {
-    onSnapshot(
-      model,
-      throttle((snap) => {
-        const url = new URL(window.document.URL);
-        url.searchParams.set("data", JSON.stringify(snap));
-        window.history.replaceState(null, "", url.toString());
-      }, 500)
-    );
-  }, [model]);
+      annots.push(elt.addAnnotation(ap.positionToVector3(), elm));
+      if (
+        elt.structure.pickingProxy &&
+        (pickingProxy.atom || pickingProxy.bond)
+      ) {
+        var atom = pickingProxy.atom || pickingProxy.closestBondAtom;
+        var mp = pickingProxy.mouse.position;
+        tooltip.innerText = "ATOM: " + atom.qualifiedName();
+        tooltip.style.bottom = window.innerHeight - mp.y + 3 + "px";
+        tooltip.style.left = mp.x + 3 + "px";
+        tooltip.style.display = "block";
+      } else {
+        tooltip.style.display = "none";
+      }
+      elt.autoView();
+    });
+    setAnnotation(annots);
+  }, [model, mouseCol]);
+
   return model.selected.length ? (
     <div>
       <Button onClick={() => model.clearSelection()} variant="contained">
@@ -106,12 +134,25 @@ const ProteinPanel = observer(({ model }) => {
         <MenuItem value={"cartoon"}>cartoon</MenuItem>
         <MenuItem value={"ball+stick"}>ball+stick</MenuItem>
       </Select>
+      <div
+        ref={tooltipRef}
+        style={{
+          display: "none",
+          position: "fixed",
+          zIndex: 10,
+          pointerEvents: "none",
+          backgroundColor: "rgba( 0, 0, 0, 0.6 )",
+          color: "lightgrey",
+          padding: "8px",
+          fontFamily: "sans-serif",
+        }}
+      />
       <div id="viewport" ref={ref} style={{ width: 600, height: 400 }} />
     </div>
   ) : null;
 });
 
-function App() {
+const App = observer(({ model }) => {
   return (
     <div>
       <div style={{ border: "1px solid black", margin: 20 }}>
@@ -120,13 +161,13 @@ function App() {
       <ProteinPanel model={model} />
     </div>
   );
-}
+});
 
 export default () => {
   const theme = createJBrowseTheme();
   return (
     <ThemeProvider theme={theme}>
-      <App />
+      <App model={global} />
     </ThemeProvider>
   );
 };
