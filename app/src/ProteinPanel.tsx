de8e38e98e7c4c14263ef22852efdf688a0f1ba4
Colin <colin.diesh@gmail.com>
2021-06-21 12:17:09 -0400

    Add stage autoView onload of structurecomponent

--- app/src/ProteinPanel.tsx
+++ app/src/ProteinPanel.tsx
@@ -3,7 +3,7 @@ import { observer } from "mobx-react";
 import { Button, Select, MenuItem, TextField } from "@material-ui/core";
 import { StaticDatasource, DatasourceRegistry } from "ngl";
 import { AppModel } from "./model";
-import { Stage, Component, useComponent, useStage } from "react-ngl";
+import { Stage, StructureComponent, useComponent, useStage } from "react-ngl";
 
 DatasourceRegistry.add(
   "data",
@@ -68,16 +68,29 @@ const ProteinElement = observer(
   ({
     model,
     isMouseHovering,
+    reprType,
+    nglSelection,
+    pdbFile,
   }: {
     model: AppModel;
     isMouseHovering: boolean;
+    reprType: string;
+    nglSelection: string;
+    pdbFile: string;
   }) => {
     const stage = useStage();
     const myListener = useCallback(() => {}, []);
+    // useEffect(() => {
+    //   if (stage) {
+    //     setTimeout(() => {
+    //       stage.autoView();
+    //     }, 100);
+    //   }
+    // }, []);
 
     const reprList = useMemo(() => {
-      return [{ type: "cartoon" }] as any;
-    }, []);
+      return [{ type: reprType }] as any;
+    }, [reprType]);
 
     useEffect(() => {
       stage.signals.hovered.add(myListener);
@@ -85,42 +98,23 @@ const ProteinElement = observer(
     }, [myListener, stage.signals.hovered]);
 
     return (
-      <Component path="rcsb://4hhb" reprList={reprList}>
+      <StructureComponent
+        path={pdbFile}
+        reprList={reprList}
+        selection={nglSelection}
+        onLoad={() => stage.autoView()}
+      >
         <ProteinComponent model={model} isMouseHovering={isMouseHovering} />
-      </Component>
+      </StructureComponent>
     );
   }
 );
 
 export const ProteinPanel = observer(({ model }: { model: AppModel }) => {
-  const [type, setType] = useState("cartoon");
+  const [reprType, setReprType] = useState("cartoon");
   const { msaview, nglSelection } = model;
   const { selectedStructures } = msaview;
   const [isMouseHovering, setMouseHovering] = useState(false);
-
-  // const stageElementRef = useCallback((element) => {
-  //   if (element) {
-  //     const currentStage = new Stage(element);
-  //     setStage(currentStage);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   return () => {
-  //     if (stage) {
-  //       stage.dispose();
-  //     }
-  //   };
-  // }, [stage]);
-
-  // useEffect(() => {
-  //   // Handle window resizing
-  //   window.addEventListener("resize", () => {
-  //     if (stage) {
-  //       stage.handleResize();
-  //     }
-  //   });
-  // }, [stage]);
 
   // useEffect(() => {
   //   (async () => {
@@ -147,17 +141,8 @@ export const ProteinPanel = observer(({ model }: { model: AppModel }) => {
   //     setRes(res);
   //   })();
   // }, [JSON.stringify(selectedStructures), stage]);
-
-  // useEffect(() => {
-  //   if (stage) {
-  //     res.forEach((elt) => {
-  //       elt.removeAllRepresentations();
-  //       elt.addRepresentation(type, { sele: nglSelection });
-  //     });
-  //     stage.autoView();
-  //   }
-  // }, [type, res, stage, nglSelection]);
-
+  //
+  //
   return selectedStructures.length ? (
     <div style={{ padding: 20 }}>
       <div style={{ display: "flex", alignItems: "center" }}>
@@ -170,8 +155,8 @@ export const ProteinPanel = observer(({ model }: { model: AppModel }) => {
 
         <div style={{ width: 20 }} />
         <Select
-          value={type}
-          onChange={(event) => setType(event.target.value as string)}
+          value={reprType}
+          onChange={(event) => setReprType(event.target.value as string)}
         >
           <MenuItem value={"cartoon"}>cartoon</MenuItem>
           <MenuItem value={"ball+stick"}>ball+stick</MenuItem>
@@ -189,7 +174,13 @@ export const ProteinPanel = observer(({ model }: { model: AppModel }) => {
         onMouseLeave={() => setMouseHovering(false)}
       >
         <Stage width="600px" height="400px">
-          <ProteinElement model={model} isMouseHovering={isMouseHovering} />
+          <ProteinElement
+            model={model}
+            reprType={reprType}
+            isMouseHovering={isMouseHovering}
+            nglSelection={nglSelection}
+            pdbFile={`data://${selectedStructures[0].structure.pdb}.pdb`}
+          />
         </Stage>
       </div>
     </div>
