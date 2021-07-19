258f6f213584db4a978c0fc34b0ed2cde3d57fed
Colin <colin.diesh@gmail.com>
2021-07-19 13:55:30 -0400

    Add mousewheel x scrolling in track area

--- lib/src/components/Track.tsx
+++ lib/src/components/Track.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import normalizeWheel from 'normalize-wheel'
+
 import { observer } from 'mobx-react'
 import { MsaViewModel } from '../model'
 import { IconButton, Menu, MenuItem } from '@material-ui/core'
@@ -57,11 +59,40 @@ const Track = observer(
   ({ model, track }: { model: MsaViewModel; track: any }) => {
     const { resizeHandleWidth } = model
     const { height } = track
+    const ref = useRef<HTMLDivElement>(null)
+    const scheduled = useRef(false)
+    const deltaX = useRef(0)
+    useEffect(() => {
+      const curr = ref.current
+      if (!curr) {
+        return
+      }
+      function onWheel(origEvent: WheelEvent) {
+        const event = normalizeWheel(origEvent)
+        deltaX.current += event.pixelX
+
+        if (!scheduled.current) {
+          scheduled.current = true
+          requestAnimationFrame(() => {
+            model.doScrollX(-deltaX.current)
+            deltaX.current = 0
+            scheduled.current = false
+          })
+        }
+        origEvent.preventDefault()
+      }
+      curr.addEventListener('wheel', onWheel)
+      return () => {
+        curr.removeEventListener('wheel', onWheel)
+      }
+    }, [model])
     return (
       <div key={track.id} style={{ display: 'flex', height }}>
         <TrackLabel model={model} track={track} />
         <div style={{ width: resizeHandleWidth }} />
-        <track.ReactComponent model={model} track={track} />
+        <div ref={ref}>
+          <track.ReactComponent model={model} track={track} />
+        </div>
       </div>
     )
   },
