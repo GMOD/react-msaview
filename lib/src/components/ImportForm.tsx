794ed1f401fb4bd56598acf01cb8d65290a6861d
Colin <colin.diesh@gmail.com>
2021-08-04 09:09:01 -0400

    Lineage colors

--- lib/src/components/ImportForm.tsx
+++ lib/src/components/ImportForm.tsx
@@ -31,6 +31,7 @@ const ListItem = ({
 export default observer(({ model }: { model: MsaViewModel }) => {
   const [msaFile, setMsaFile] = useState<FileLocation>()
   const [treeFile, setTreeFile] = useState<FileLocation>()
+  const [lineageFile, setLinearFile] = useState<FileLocation>()
   const { error } = model
 
   return (
@@ -59,6 +60,8 @@ export default observer(({ model }: { model: MsaViewModel }) => {
           <FileSelector location={msaFile} setLocation={setMsaFile} />
           <Typography>Tree file or URL</Typography>
           <FileSelector location={treeFile} setLocation={setTreeFile} />
+          <Typography>Lineage colors file</Typography>
+          <FileSelector location={lineageFile} setLocation={setLinearFile} />
         </Grid>
 
         <Grid item>
@@ -82,91 +85,97 @@ export default observer(({ model }: { model: MsaViewModel }) => {
 
         <Grid item>
           <Typography>Examples</Typography>
-          <ul>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setTreeFilehandle({
-                  uri: 'https://jbrowse.org/genomes/newick_trees/sarscov2phylo.pub.ft.nh',
-                })
-              }}
-            >
-              230k COVID-19 samples (tree only)
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setData({ msa: smallMSA, tree: smallTree })
-              }}
-            >
-              Small protein MSA+tree
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setData({ msa: smallMSAOnly })
-              }}
-            >
-              Small MSA only
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setMSAFilehandle({
-                  uri: 'https://ihh.github.io/abrowse/build/pfam-cov2.stock',
-                })
-              }}
-            >
-              PFAM SARS-CoV2 multi-stockholm
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setMSAFilehandle({
-                  uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/Lysine.stock',
-                })
-              }}
-            >
-              Lysine stockholm file
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setMSAFilehandle({
-                  uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/PF01601_full.txt',
-                })
-              }}
-            >
-              PF01601 stockholm file (SARS-CoV2 spike protein)
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                model.setMSAFilehandle({
-                  uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/europe_covid.fa',
-                })
-              }}
-            >
-              Europe COVID full genomes (LR883044.1 and 199 other sequences)
-            </ListItem>
-            <ListItem
-              model={model}
-              onClick={() => {
-                transaction(() => {
-                  model.setMSAFilehandle({
-                    uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/rhv_test-only.aligned_with_mafft_auto.fa',
-                  })
-                  model.setTreeFilehandle({
-                    uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/rhv_test-only.aligned_with_mafft_auto.nh',
-                  })
-                })
-              }}
-            >
-              MAFFT+VeryFastTree(17.9k samples)
-            </ListItem>
-          </ul>
+          <Examples model={model} />
         </Grid>
       </Grid>
     </Container>
   )
 })
+
+const Examples = ({ model }: { model: MsaViewModel }) => {
+  return (
+    <ul>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setTreeFilehandle({
+            uri: 'https://jbrowse.org/genomes/newick_trees/sarscov2phylo.pub.ft.nh',
+          })
+        }}
+      >
+        230k COVID-19 samples (tree only)
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setData({ msa: smallMSA, tree: smallTree })
+        }}
+      >
+        Small protein MSA+tree
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setData({ msa: smallMSAOnly })
+        }}
+      >
+        Small MSA only
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setMSAFilehandle({
+            uri: 'https://ihh.github.io/abrowse/build/pfam-cov2.stock',
+          })
+        }}
+      >
+        PFAM SARS-CoV2 multi-stockholm
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setMSAFilehandle({
+            uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/Lysine.stock',
+          })
+        }}
+      >
+        Lysine stockholm file
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setMSAFilehandle({
+            uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/PF01601_full.txt',
+          })
+        }}
+      >
+        PF01601 stockholm file (SARS-CoV2 spike protein)
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          model.setMSAFilehandle({
+            uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/europe_covid.fa',
+          })
+        }}
+      >
+        Europe COVID full genomes (LR883044.1 and 199 other sequences)
+      </ListItem>
+      <ListItem
+        model={model}
+        onClick={() => {
+          transaction(() => {
+            model.setMSAFilehandle({
+              uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/rhv_test-only.aligned_with_mafft_auto.fa',
+            })
+            model.setTreeFilehandle({
+              uri: 'https://jbrowse.org/genomes/multiple_sequence_alignments/rhv_test-only.aligned_with_mafft_auto.nh',
+            })
+          })
+        }}
+      >
+        MAFFT+VeryFastTree(17.9k samples)
+      </ListItem>
+    </ul>
+  )
+}
