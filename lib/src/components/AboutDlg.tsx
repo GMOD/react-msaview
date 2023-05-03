3e738ea2ee1d1865aff171357a6d6ab0756f003e
Colin <colin.diesh@gmail.com>
2023-05-03 09:26:08 -0600

    Don't import package.json directly

--- lib/src/components/AboutDlg.tsx
+++ lib/src/components/AboutDlg.tsx
@@ -1,53 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Dialog } from '@jbrowse/core/ui'
 import { DialogContent, Typography, Link } from '@mui/material'
-import packageJSON from './package.json'
+import { version } from '../version'
 
-function LicenseDialog({
-  onClose,
-  open,
-}: {
-  onClose: () => void
-  open: boolean
-}) {
-  return (
-    <Dialog onClose={() => onClose()} open={open} title="Biotite license">
-      <DialogContent>
-        <pre style={{ height: 100, overflow: 'auto' }}>
-          {`
-Copyright 2017 - 2020, The Biotite contributors
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        `}
-        </pre>
-      </DialogContent>
-    </Dialog>
-  )
-}
 export default function AboutDialog({
   onClose,
   open,
@@ -55,59 +10,55 @@ export default function AboutDialog({
   onClose: () => void
   open: boolean
 }) {
-  const [dlgOpen, setDlgOpen] = useState(false)
   return (
-    <>
-      <LicenseDialog onClose={() => setDlgOpen(false)} open={dlgOpen} />
-      <Dialog onClose={() => onClose()} open={open} title="About this plugin">
-        <DialogContent>
-          <Typography>
-            MSAView {packageJSON.version} (
-            <Link href="https://github.com/gmod/jbrowse-plugin-msaview">
-              Github
-            </Link>
-            )
-          </Typography>
+    <Dialog onClose={() => onClose()} open={open} title="About this plugin">
+      <DialogContent>
+        <Typography>
+          MSAView {version} (
+          <Link href="https://github.com/gmod/jbrowse-plugin-msaview">
+            Github
+          </Link>
+          )
+        </Typography>
 
-          <ul>
-            <li>
-              <Typography>
-                We use some color schemes from the{' '}
-                <Link href="https://github.com/biotite-dev/biotite">
-                  biotite
-                </Link>{' '}
-                project, and their license is reproduced{' '}
-                <Link onClick={() => setDlgOpen(true)}>here</Link>
-              </Typography>
-            </li>
-            <li>
-              <Typography>
-                See this page for some information on jalview colorings{' '}
-                <Link href="https://www.jalview.org/help/html/colourSchemes/">
-                  here
-                </Link>
-              </Typography>
-            </li>
-            <li>
-              <Typography>
-                See this page for some info on the clustal, cinema, maeditor,
-                and lesk color schemes{' '}
-                <Link href="http://www.bioinformatics.nl/~berndb/aacolour.html">
-                  here
-                </Link>
-              </Typography>
-            </li>
-            <li>
-              <Typography>
-                See this paper about the flower color scheme{' '}
-                <Link href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7245768/">
-                  here
-                </Link>
-              </Typography>
-            </li>
-          </ul>
-        </DialogContent>
-      </Dialog>
-    </>
+        <ul>
+          <li>
+            <Typography>
+              We use some color schemes from the{' '}
+              <Link href="https://github.com/biotite-dev/biotite">biotite</Link>{' '}
+              project, and their license is reproduced{' '}
+              <Link href="https://github.com/biotite-dev/biotite/blob/master/LICENSE.rst">
+                here
+              </Link>
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              See this page for some information on jalview colorings{' '}
+              <Link href="https://www.jalview.org/help/html/colourSchemes/">
+                here
+              </Link>
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              See this page for some info on the clustal, cinema, maeditor, and
+              lesk color schemes{' '}
+              <Link href="http://www.bioinformatics.nl/~berndb/aacolour.html">
+                here
+              </Link>
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              See this paper about the flower color scheme{' '}
+              <Link href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7245768/">
+                here
+              </Link>
+            </Typography>
+          </li>
+        </ul>
+      </DialogContent>
+    </Dialog>
   )
 }
