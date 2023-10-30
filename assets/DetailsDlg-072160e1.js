0eb8a7f9d96d142439c207e783a347572a0cf764
Colin <colin.diesh@gmail.com>
2023-10-29 21:49:11 -0400

    Updates

--- assets/DetailsDlg-072160e1.js
+++
@@ -1,2 +0,0 @@
-import{o as r,R as e,u as s,D as i}from"./index-56495e8d.js";import{B as l}from"./index-95699cbd.js";import"./index-93f78a20.js";const c=r(function({model:t,onClose:a}){const{header:o}=t;return e.createElement(s.Dialog,{onClose:()=>a(),open:!0,title:"Metadata"},e.createElement(i,null,e.createElement(l.Attributes,{attributes:o})))});export{c as default};
-//# sourceMappingURL=DetailsDlg-072160e1.js.map
--- assets/MoreInfoDlg-35ba1eeb.js
+++
@@ -1,2 +0,0 @@
-import{o,R as e,u as r,D as n}from"./index-56495e8d.js";import{B as l}from"./index-95699cbd.js";import"./index-93f78a20.js";const m=o(function({info:t,onClose:a}){return e.createElement(r.Dialog,{onClose:()=>a(),open:!0,title:"Metadata"},e.createElement(n,null,e.createElement(l.Attributes,{attributes:t})))});export{m as default};
-//# sourceMappingURL=MoreInfoDlg-35ba1eeb.js.map
