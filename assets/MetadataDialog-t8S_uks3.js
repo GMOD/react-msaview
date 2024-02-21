db73a218c5fdbcac4aa04f83c4142155af009cf2
Colin <colin.diesh@gmail.com>
2024-02-21 16:35:42 -0500

    Updates

---
+++ assets/MetadataDialog-t8S_uks3.js
@@ -0,0 +1,2 @@
+import{o as r,R as e,u as n,D as s}from"./index-D3BxRj96.js";import{B as l}from"./index-DrQDdWjW.js";const c=r(function({model:t,onClose:a}){const{header:o}=t;return e.createElement(n.Dialog,{onClose:()=>a(),open:!0,title:"Metadata"},e.createElement(s,null,e.createElement(l.Attributes,{attributes:o})))});export{c as default};
+//# sourceMappingURL=MetadataDialog-t8S_uks3.js.map
