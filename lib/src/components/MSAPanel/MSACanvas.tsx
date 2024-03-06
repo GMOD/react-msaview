2b508aa1ea3d53e6ffabd9df64c7a0f8c91a288a
Colin <colin.diesh@gmail.com>
2024-03-06 01:38:39 -0500

    Attempt2

--- lib/src/components/MSAPanel/MSACanvas.tsx
+++ lib/src/components/MSAPanel/MSACanvas.tsx
@@ -1,43 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import 'pixi.js/text-bitmap'
+import React, { useEffect, useRef, useState } from 'react'
 import { observer } from 'mobx-react'
 import normalizeWheel from 'normalize-wheel'
-import * as PIXI from 'pixi.js'
-import { Assets } from 'pixi.js'
-import { Stage, Container, Sprite, Text, BitmapText } from '@pixi/react'
+import { Application, Assets, Sprite } from 'pixi.js'
 // locals
 import { MsaViewModel } from '../../model'
 import MSABlock from './MSABlock'
 import Loading from './Loading'
 
 // Helper Component to ensure assets are loaded for docusaurus live examples
-const ExampleAssetLoader = ({
-  name,
-  url,
-  loader,
-  children,
-}: {
-  name: string
-  url: string
-  loader: React.ReactNode
-  children: React.ReactNode
-}) => {
-  const [isLoaded, setIsLoaded] = useState(false)
+async function loadFont({ name, url }: { name: string; url: string }) {
+  Assets.add({
+    alias: name,
+    src: url,
+  })
 
-  useEffect(() => {
-    const loadAsset = async () => {
-      Assets.add({
-        alias: name,
-        src: url,
-      })
-
-      await Assets.load(name)
-      setIsLoaded(true)
-    }
-
-    loadAsset().catch(console.error)
-  }, [name, url])
-
-  return isLoaded ? children : loader
+  await Assets.load(name)
 }
 
 const MSACanvas = observer(function MSACanvas2({
@@ -46,36 +24,53 @@ const MSACanvas = observer(function MSACanvas2({
   model: MsaViewModel
 }) {
   const { msaAreaWidth, height } = model
-
-  return (
-    <Stage width={msaAreaWidth} height={height} options={{ hello: true }}>
-      <ExampleAssetLoader
-        name="sans-serif"
-        url="https://s3.amazonaws.com/jbrowse.org/demos/font/sans-serif.xml"
-        loader={
-          <Text
-            x={100}
-            y={100}
-            text="⌛ Loading font..."
-            style={
-              new PIXI.TextStyle({
-                fill: ['#ffffff'],
-              })
-            }
-          />
+  const ref = useRef<HTMLCanvasElement>(null)
+  const completed = useRef(false)
+  const [error, setError] = useState<unknown>()
+  useEffect(() => {
+    if (completed.current) {
+      return
+    }
+    completed.current = true
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    ;(async () => {
+      try {
+        if (!ref.current) {
+          return
         }
-      >
-        <BitmapText
-          x={100}
-          y={100}
-          text="Hello World!"
-          style={{
-            fontName: 'sans-serif',
-            fontSize: 72,
-          }}
-        />
-      </ExampleAssetLoader>
-    </Stage>
+
+        await loadFont({
+          name: 'sans-serif',
+          url: 'https://s3.amazonaws.com/jbrowse.org/demos/font/sans-serif.xml',
+        })
+
+        const app = new Application()
+        await app.init({
+          width: msaAreaWidth,
+          height,
+          canvas: ref.current,
+        })
+
+        const texture = await Assets.load('sample.png')
+        let sprite = Sprite.from(texture)
+        app.stage.addChild(sprite)
+
+        // Add a ticker callback to move the sprite back and forth
+        let elapsed = 0.0
+        app.ticker.add(ticker => {
+          elapsed += ticker.deltaTime
+          sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0
+        })
+      } catch (e) {
+        setError(e)
+        console.error(e)
+      }
+    })()
+  }, [])
+  return error ? (
+    <div style={{ color: 'red' }}>{`${error}`}</div>
+  ) : (
+    <canvas ref={ref} />
   )
 })
 
