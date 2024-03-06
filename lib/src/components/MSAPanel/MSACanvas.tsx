350293263c9bdb4b2fea9b57f4c5ef91252608a5
Colin <colin.diesh@gmail.com>
2024-03-06 12:32:16 -0500

    Font

--- lib/src/components/MSAPanel/MSACanvas.tsx
+++ lib/src/components/MSAPanel/MSACanvas.tsx
@@ -2,11 +2,12 @@ import 'pixi.js/text-bitmap'
 import React, { useEffect, useRef, useState } from 'react'
 import { observer } from 'mobx-react'
 import normalizeWheel from 'normalize-wheel'
-import { Application, Assets, Sprite } from 'pixi.js'
+import { Application, Assets, BitmapFont, BitmapText, Sprite } from 'pixi.js'
 // locals
 import { MsaViewModel } from '../../model'
 import MSABlock from './MSABlock'
 import Loading from './Loading'
+import { ErrorMessage } from '@jbrowse/core/ui'
 
 // Helper Component to ensure assets are loaded for docusaurus live examples
 async function loadFont({ name, url }: { name: string; url: string }) {
@@ -18,6 +19,27 @@ async function loadFont({ name, url }: { name: string; url: string }) {
   await Assets.load(name)
 }
 
+const useBitmapFontLoader = () => {
+  const [error, setError] = useState<unknown>()
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    ;(async () => {
+      try {
+        setLoading(true)
+        await loadFont({
+          name: 'sans-serif',
+          url: 'https://s3.amazonaws.com/jbrowse.org/demos/font/sans-serif.xml',
+        })
+        setLoading(false)
+      } catch (e) {
+        setError(e)
+      }
+    })()
+  }, [])
+  return { fontError: error, fontLoading: loading }
+}
+
 const MSACanvas = observer(function MSACanvas2({
   model,
 }: {
@@ -27,50 +49,60 @@ const MSACanvas = observer(function MSACanvas2({
   const ref = useRef<HTMLCanvasElement>(null)
   const completed = useRef(false)
   const [error, setError] = useState<unknown>()
+  const { fontError, fontLoading } = useBitmapFontLoader({
+    name: 'sans-serif',
+    url: 'https://s3.amazonaws.com/jbrowse.org/demos/font/sans-serif.xml',
+  })
   useEffect(() => {
-    if (completed.current) {
-      return
-    }
-    completed.current = true
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     ;(async () => {
       try {
         if (!ref.current) {
           return
         }
-
-        await loadFont({
-          name: 'sans-serif',
-          url: 'https://s3.amazonaws.com/jbrowse.org/demos/font/sans-serif.xml',
-        })
-
         const app = new Application()
         await app.init({
           width: msaAreaWidth,
           height,
           canvas: ref.current,
+          resizeTo: ref.current,
         })
 
-        const texture = await Assets.load('sample.png')
-        let sprite = Sprite.from(texture)
-        app.stage.addChild(sprite)
+        const ret = [] as BitmapText[]
+        for (let i = 0; i < 100; i++) {
+          const x = new BitmapText({
+            text: 'word',
+            x: msaAreaWidth * Math.random(),
+            y: height * Math.random(),
+            style: {
+              fontFamily: 'sans-serif',
+              fontSize: 20,
+            },
+          })
+          ret.push(x)
+          app.stage.addChild(x)
+        }
 
         // Add a ticker callback to move the sprite back and forth
-        let elapsed = 0.0
         app.ticker.add(ticker => {
-          elapsed += ticker.deltaTime
-          sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0
+          for (const elt of ret) {
+            elt.updateTransform({
+              x: elt.x + (Math.random() - 0.5) * 1,
+              y: elt.y + (Math.random() - 0.5) * 1,
+            })
+          }
         })
       } catch (e) {
         setError(e)
         console.error(e)
       }
     })()
-  }, [])
-  return error ? (
-    <div style={{ color: 'red' }}>{`${error}`}</div>
+  }, [height, msaAreaWidth])
+  const e = error || fontError
+  return e ? (
+    <ErrorMessage error={e} />
   ) : (
-    <canvas ref={ref} />
+    <canvas width={msaAreaWidth} height={height} ref={ref} />
   )
 })
 
