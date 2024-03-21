464c2a9a5c899d787645a5e8393921e0573b29cf
Colin <colin.diesh@gmail.com>
2024-03-21 00:06:16 -0400

    Updates

--- assets/MetadataDialog-CG0IU1O_.js
+++
@@ -1,2 +0,0 @@
-import{o as r,R as e,k as n,D as s}from"./index-yx-9MxwX.js";import{B as l}from"./index-BAq0vo4N.js";const c=r(function({model:t,onClose:a}){const{header:o}=t;return e.createElement(n.Dialog,{onClose:()=>a(),open:!0,title:"Metadata"},e.createElement(s,null,e.createElement(l.Attributes,{attributes:o})))});export{c as default};
-//# sourceMappingURL=MetadataDialog-CG0IU1O_.js.map
