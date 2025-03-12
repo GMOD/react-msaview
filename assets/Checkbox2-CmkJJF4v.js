656732ea640ca27913387f47885b75525557b0d7
Colin <colin.diesh@gmail.com>
2025-03-12 03:21:54 -0400

    Updates

---
+++ assets/Checkbox2-CmkJJF4v.js
@@ -0,0 +1,2 @@
+import{R as e,C as a,h as l}from"./index-DPWBesFv.js";function c(t){return e.createElement("div",null,e.createElement(l,{...t}))}function C({checked:t,label:n,disabled:o,onChange:r}){return e.createElement(c,{control:e.createElement(a,{disabled:o,checked:t,onChange:r}),label:n})}export{C};
+//# sourceMappingURL=Checkbox2-CmkJJF4v.js.map
