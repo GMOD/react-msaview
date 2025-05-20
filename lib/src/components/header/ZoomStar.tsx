3fa162d7a63c640ec6e289cb0829c85587e6f15d
Colin <colin.diesh@gmail.com>
2025-05-20 14:43:55 -0400

    Styles on zoom star

--- lib/src/components/header/ZoomStar.tsx
+++ lib/src/components/header/ZoomStar.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-import ArrowDropDown from '@mui/icons-material/ArrowDropDown'
-import ArrowDropUp from '@mui/icons-material/ArrowDropUp'
-import ArrowLeft from '@mui/icons-material/ArrowLeft'
-import ArrowRight from '@mui/icons-material/ArrowRight'
-import ZoomIn from '@mui/icons-material/ZoomIn'
-import ZoomOut from '@mui/icons-material/ZoomOut'
 import { IconButton } from '@mui/material'
 import { observer } from 'mobx-react'
 import { makeStyles } from 'tss-react/mui'
@@ -14,17 +8,15 @@ import { RestartAlt } from '@mui/icons-material'
 
 import type { MsaViewModel } from '../../model'
 
-const useStyles = makeStyles()({
+const useStyles = makeStyles()(theme => ({
   dpad: {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
-    margin: 0,
   },
   icon: {
-    padding: 0,
-    margin: 0,
+    padding: theme.spacing(0.5),
   },
-})
+}))
 
 const ZoomStar = observer(function ({ model }: { model: MsaViewModel }) {
   const { classes } = useStyles()
@@ -50,6 +42,7 @@ const ZoomStar = observer(function ({ model }: { model: MsaViewModel }) {
         X-
       </IconButton>
       <IconButton
+        className={classes.icon}
         onClick={() => {
           model.resetZoom()
         }}
