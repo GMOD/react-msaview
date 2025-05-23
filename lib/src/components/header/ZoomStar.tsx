b1fb50164bc0d2237827a4b9836c59e6eea1efc7
Colin <colin.diesh@gmail.com>
2025-05-23 02:04:40 -0400

    Update

--- lib/src/components/header/ZoomStar.tsx
+++ lib/src/components/header/ZoomStar.tsx
@@ -4,7 +4,7 @@ import { IconButton } from '@mui/material'
 import { observer } from 'mobx-react'
 import { makeStyles } from 'tss-react/mui'
 
-import { RestartAlt } from '@mui/icons-material'
+import RestartAlt from '@mui/icons-material/RestartAlt'
 
 import type { MsaViewModel } from '../../model'
 
